973565a2ed1a6f4ff3141b2e102d6f93b9b321d1
asaduzzaman2189 <asaduzzaman2189@gmail.com>
2021-08-22 14:20:52 +0600

    27-7 Verify match and use function to reduce duplicate

--- js/app.js
+++ js/app.js
@@ -28,3 +28,18 @@ document.getElementById('key-pad').addEventListener('click', function (event) {
         calcInput.value = newNumber;
     }
 })
+
+function verifyPin() {
+    const pin = document.getElementById('display-pin').value;
+    const typedNumbers = document.getElementById('typed-numbers').value;
+    const failError = document.getElementById('notify-fail');
+    const successMessage = document.getElementById('notify-success');
+    if (pin != typedNumbers) {
+        failError.style.display = 'block'; successMessage.style.display = 'none';
+
+    }
+    else {
+        successMessage.style.display = 'block';
+        failError.style.display = 'none';
+    }
+}
